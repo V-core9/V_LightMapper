3eb580d51c6f6d64b14117b184523ceabcdf36ae
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-12-31 03:00:01 +0100

    yeaaa

--- run.js
+++ run.js
@@ -1,4 +1,12 @@
-const vLM = require('.');
-const vFS = require('v_file_system');
+const v_lightmapper = require('.');
 
-vLM(JSON.parse(vFS.readSy('./config.json')));
+const config = {
+    protocol: "https",
+    host: "v-core9.com",
+    path: "sitemap_index.xml",
+    save_to_file: true,
+    disableHeadlessMode: false,
+    onlyCategories : ["performance"]
+};
+
+v_lightmapper(config);
